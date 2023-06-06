93322627d12199fd7b4da27e9cf218812be774ee
YunfanKang <ykang040@ucr.edu>
2023-06-06 02:38:23 -0700

    prepare for pip

---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup
+
+def _get_requirements_from_files(groups_files):
+    groups_reqlist = {}
+
+    for k, v in groups_files.items():
+        with open(v, "r") as f:
+            pkg_list = f.read().splitlines()
+        groups_reqlist[k] = pkg_list
+
+    return groups_reqlist
+
+setup(
+   name='Pyneapple',
+   version='0.0.1',
+   description='Scalable and expressive spatial analysis',
+   author='Yunfan Kang',
+   author_email='ykang040@ucr.edu',
+   packages=['Pyneapple'],  #same as name
+   install_requires=['wheel', 'bar', 'greek'], #external packages as dependencies
+)
+
+def setup_package():
+   _groups_files = {
+      "base": "requirements.txt",
+      # "tests": "requirements_tests.txt",
+      #  "docs": "requirements_docs.txt",
+   }
+
+   reqs = _get_requirements_from_files(_groups_files)
+   install_reqs = reqs.pop("base")
+   #extras_reqs = reqs
+
+   setup(
+   name='pyneapple',
+   version='0.0.1',
+   description='Scalable and expressive spatial analysis',
+   author='Yunfan Kang',
+   author_email='ykang040@ucr.edu',
+   packages=['pyneapple'],  #same as name
+   install_requires=install_reqs, #external packages as dependencies
+)
+
+
+if __name__ == "__main__":
+    setup_package()
