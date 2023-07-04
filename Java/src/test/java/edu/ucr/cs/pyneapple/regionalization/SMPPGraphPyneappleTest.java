17b61ed0b4595e260fd1faa5b842283efa0a3363
Hussah <57476374+Hussah-UCR@users.noreply.github.com>
2023-07-04 13:06:40 -0700

    Add files via upload

---
+++ Java/src/test/java/edu/ucr/cs/pyneapple/regionalization/SMPPGraphPyneappleTest.java
@@ -0,0 +1,82 @@
+package edu.ucr.cs.pyneapple.regionalization;
+
+import junit.framework.TestCase;
+import org.locationtech.jts.geom.Geometry;
+
+import java.util.*;
+
+public class SMPPGraphPyneappleTest extends TestCase {
+
+    static String shp_file = "data/LACity/LACity.shp";
+
+    public SMPPGraphPyneappleTest(){}
+
+
+    /**
+     * The test case for execute_regionalization() function
+     * @throws Exception Exception when the shapefile cannot be loaded or the attributes of the corresponding name does not exist in the shapefile.
+     */
+    public void test_execute_regionalization() throws Exception {
+
+        SMPPGraphPyneapple smpp = new SMPPGraphPyneapple();
+
+        ArrayList<String> string_polygons = new ArrayList<>();
+        ArrayList<Geometry> areas_polygons = new ArrayList<>();
+        ArrayList<Integer> areas_IDs = new ArrayList<>();
+        ArrayList<Double> diss_attr = new ArrayList<>();
+        ArrayList<Double> ex_attr = new ArrayList<>();
+        smpp.read_files(shp_file, string_polygons, areas_polygons, areas_IDs, diss_attr, ex_attr, "AWATER", "ALAND");
+
+        Map<Integer, Set<Integer>> neighbors = smpp.createNeighborsList(areas_polygons);
+
+        smpp.execute_regionalization(4, 2, 2, 250000000, 50, 100, 1, 0.9, 50, 0, diss_attr, ex_attr, neighbors);
+
+    }
+
+
+    /**
+     * The test case for the getP() interface. The function should print the P value after the regionalization computation.
+     * @throws Exception Exception when the shapefile cannot be loaded or the attributes of the corresponding name does not exist in the shapefile.
+     */
+    public void test_getP() throws Exception {
+
+        SMPPGraphPyneapple smpp = new SMPPGraphPyneapple();
+
+        ArrayList<String> string_polygons = new ArrayList<>();
+        ArrayList<Geometry> areas_polygons = new ArrayList<>();
+        ArrayList<Integer> areas_IDs = new ArrayList<>();
+        ArrayList<Double> diss_attr = new ArrayList<>();
+        ArrayList<Double> ex_attr = new ArrayList<>();
+        smpp.read_files(shp_file, string_polygons, areas_polygons, areas_IDs, diss_attr, ex_attr, "AWATER", "ALAND");
+
+        Map<Integer, Set<Integer>> neighbors = smpp.createNeighborsList(areas_polygons);
+
+        smpp.execute_regionalization(4, 2, 2, 250000000, 50, 100, 1, 0.9, 50, 0, diss_attr, ex_attr, neighbors);
+
+        System.out.println(smpp.getP());
+
+    }
+
+
+    /**
+     * The test case for the getRegionList() interface. An array storing the region lable of each area should be printed out.
+     * @throws Exception Exception when the shapefile cannot be loaded or the attributes of the corresponding name does not exist in the shapefile.
+     */
+    public void test_getRegionLabels() throws Exception {
+        SMPPGraphPyneapple smpp = new SMPPGraphPyneapple();
+
+        ArrayList<String> string_polygons = new ArrayList<>();
+        ArrayList<Geometry> areas_polygons = new ArrayList<>();
+        ArrayList<Integer> areas_IDs = new ArrayList<>();
+        ArrayList<Double> diss_attr = new ArrayList<>();
+        ArrayList<Double> ex_attr = new ArrayList<>();
+        smpp.read_files(shp_file, string_polygons, areas_polygons, areas_IDs, diss_attr, ex_attr, "AWATER", "ALAND");
+
+        Map<Integer, Set<Integer>> neighbors = smpp.createNeighborsList(areas_polygons);
+
+        smpp.execute_regionalization(4, 2, 2, 250000000, 50, 100, 1, 0.9, 50, 0, diss_attr, ex_attr, neighbors);
+        System.out.println(Arrays.toString(smpp.getRegionLabels()));
+    }
+
+
+}
