0a87021b417b2dc159218036ab145d7de96f4e49
YunfanKang <ykang040@ucr.edu>
2023-07-13 11:00:11 +0800

    Update MaxP.py

--- pyneapple/regionalization/MaxP.py
+++ pyneapple/regionalization/MaxP.py
@@ -12,6 +12,73 @@ import spopt
 from spopt.region import maxp as MaxP
 
 def maxp(df, w, disName, sumName = None, sumLow = -math.inf, sumHigh = math.inf, minName = None, minLow = -math.inf, minHigh = math.inf, maxName = None, maxLow = -math.inf, maxHigh = math.inf, avgName = None, avgLow = -math.inf, avgHigh = math.inf, countLow = -math.inf, countHigh = math.inf):
+    """The API for the max-p regions algorithms. If only the parameters for the max-p regions problem are privided, it will call the scalable max-p algorithm. Otherwise, the expressive max-p regions algorithm is invoked.
+
+    Parameters
+    ----------
+
+    df : geopandas.GeoDataFrame, required
+        Geodataframe containing original data
+
+    w : libpysal.weights.W, required
+        Weights object created from given data
+
+    disname : String, required
+        Strings for attribute names to measure similarity (cols of ``geopandas.GeoDataFrame``).
+
+    minName : string, requied
+        The name of the spatial extensive attribute variable for the MIN constraint.
+
+    minLow : {int, float}, required
+        The lowerbound for the MIN range.
+
+    minHigh : {int, float}, required
+        The upperbound for the MIN range.
+
+    maxName : string, requied
+        The name of the spatial extensive attribute variable for the MAX constraint.
+
+    maxLow : {int, float}, required
+        The lowerbound for the MAX range.
+
+    maxHigh : {int, float}, required
+        The upperbound for the MAX range.
+
+    avgName : string, requied
+        The name of the spatial extensive attribute variable for the AVG constraint.
+
+    avgLow : {int, float}, required
+        The lowerbound for the AVG range.
+
+    avgHigh : {int, float}, required
+        The upperbound for the AVG range.
+
+    sumName : string, requied
+        The name of the spatial extensive attribute variable for the SUM constraint.
+
+    sumLow : {int, float}, required
+        The lowerbound for the SUM range.
+
+    sumHigh : {int, float}, required
+        The upperbound for the SUM range.
+
+    countLow : {int, float}, required
+        The lowerbound for the COUNT range.
+
+    countHigh : {int, float}, required
+        The upperbound for the COUNT range.
+
+
+    Returns
+    -------
+
+    max_p : int
+        The number of regions.
+
+    labels : numpy.array
+        Region IDs for observations.
+
+    """
     EMP_flag = False
     if(minLow != -float('inf') or minHigh != float('inf')):
         if(minName == None):
